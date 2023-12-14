b81970ddc9bc6a40bd791cb555c9fa6d17dcf7c0
Rajitha9573 <rajithabhukya967@gmail.com>
2023-12-14 19:56:05 +0530

    tryg

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [name, setName] = useState("")
+
+  function updateName(event){
+     let name = setName(event.target.value);
+    console.log(name);
+
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label id="name">Enter your name:</label><br></br>
+        <input type="text" onChange={updateName}/>
+        <div>Hello {name}!</div>
     </div>
+    
   )
 }
 
