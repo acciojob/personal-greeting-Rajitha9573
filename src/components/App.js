f153557c9cbf9e78ccacb79197daff421216f392
Rajitha9573 <rajithabhukya967@gmail.com>
2024-03-04 20:49:07 +0530

    jjjg

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <label id="name">Enter your name:</label><br/>
         <input type="text" value = {name} onChange={updateName}/>
-        <div>Hello {name}!</div>
+        <p>Hello {name}!</p>
     </div>
     
   )
